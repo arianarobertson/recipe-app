c1eac4106577d54e52fedff02e41a8e8ac6d2e23
arianarobertson <arobertson22@berkeley.edu>
2025-08-29 21:13:28 -0700

    Add Heroku domain to ALLOWED_HOSTS

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'immense-beach-49370-1ee7eb454b6b.herokuapp.com']
 
 
 # Application definition
